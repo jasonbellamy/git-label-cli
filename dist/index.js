add3df084d4076e86588d7633c90b8e0abb8843c
Jason Bellamy <j@sonbellamy.com>
2016-01-04 16:51:56 -0500

    Add interactive prompt support

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict';
+
+var _commander = require('commander');
+
+var _commander2 = _interopRequireDefault(_commander);
+
+var _gitLabel = require('git-label');
+
+var _run = require('./util/run');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_commander2.default.option('-i, --interactive', 'run in interactive mode').option('-a, --api <api>', 'api url').option('-t, --token <token>', 'api token').option('-r, --repo <repo>', 'repo name [username/repo]').option('-p, --packages <items>', 'comma delimited path(s) to a label package', function (val) {
+  return val.split(',');
+}, []);
+
+_commander2.default.command('add').description('Add the specified labels to a repo').action(function () {
+  return (0, _run.run)(_gitLabel.add, _commander2.default.interactive, _commander2.default).then(console.log).catch(console.log);
+});
+
+_commander2.default.command('remove').description('Remove the specified labels from a repo').action(function () {
+  return (0, _run.run)(_gitLabel.remove, _commander2.default.interactive, _commander2.default).then(console.log).catch(console.log);
+});
+
+_commander2.default.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 #!/usr/bin/env node
 
-var program  = require('commander');
-var gitLabel = require('git-label');
-var pkg      = require('../package.json');
+import program from 'commander';
+import {add, remove} from 'git-label';
+import {run} from './util/run';
 
 
 program
-  .version(pkg.version)
+  .option('-i, --interactive', 'run in interactive mode')
   .option('-a, --api <api>', 'api url')
   .option('-t, --token <token>', 'api token')
   .option('-r, --repo <repo>', 'repo name [username/repo]')
-  .option('-p, --packages <items>', 'comma delimited path(s) to a label package', (val) => val.split(','), [])
+  .option('-p, --packages <items>', 'comma delimited path(s) to a label package', (val) => val.split(','), []);
 
 program
   .command('add')
   .description('Add the specified labels to a repo')
-  .action(function() {
-    gitLabel.add({api: program.api, token: program.token, repo: program.repo}, program.packages)
-      .then(console.log)
-      .catch(console.log);
-  });
+  .action(() => run(add, program.interactive, program).then(console.log).catch(console.log));
 
 program
   .command('remove')
   .description('Remove the specified labels from a repo')
-  .action(function() {
-    gitLabel.remove({api: program.api, token: program.token, repo: program.repo}, program.packages)
-      .then(console.log)
-      .catch(console.log);
-  });
+  .action(() => run(remove, program.interactive, program).then(console.log).catch(console.log));
 
 program.parse(process.argv);
